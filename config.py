f90e5981c59b1876a926d19afb7442e36ba03ab7
rohit340000 <164490543+rohit340000@users.noreply.github.com>
2024-05-18 14:33:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 29707621
-api_hash = "a1f31eeb31a0b9e36f551da6b8b85752"
-bot_token = "6634971116:AAGhU6IQBREjXdliqJr9CGh1ZCLpoYbGP5o"
-auth_users = "6707073614, 1406300051, 6801378994, 870101201"
+api_id = 27498866
+api_hash = "96fbb6ad2e11ab04e83ca09ef3f42455"
+bot_token = "6528420500:AAH0-q4m1mytX1rLdG2x-qJkPBdofzHo3fc"
+auth_users = "6488911325"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6707073614"
+osowner_users = "6488911325"
 owner_users = [int(num) for num in osowner_users.split(",")]
