886a40dbce8b4d4302fafb806fc76a1b2506f925
STuHacker <132763286+rihankumar@users.noreply.github.com>
2024-05-19 14:13:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 27498866
-api_hash = "96fbb6ad2e11ab04e83ca09ef3f42455"
-bot_token = "6528420500:AAH0-q4m1mytX1rLdG2x-qJkPBdofzHo3fc"
-auth_users = "6488911325"
+api_id = 4152
+api_hash = ""
+bot_token = "6528fc"
+auth_users = ""
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6488911325"
+osowner_users = ""
 owner_users = [int(num) for num in osowner_users.split(",")]
